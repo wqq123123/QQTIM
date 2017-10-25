c842b29397eb4f79c538c8753781173c7d66c578
wangqq <18735161290@163.com>
2017-10-25 20:28:42 +0800

    第一次练习

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by acer-pc on 2017/10/9.
+ */
+$(function(){
+    $.stellar({
+        //横向无滚动条
+        horizontalScrolling:false,
+        //页面尺寸发生改变,无刷新加载
+        responsive:true
+    });
+});
---
+++ js/index1.js
@@ -0,0 +1,12 @@
+/**
+ * Created by acer-pc on 2017/10/9.
+ */
+$(function(){
+    $.stellar({
+        //横向无滚动条
+        horizontalScroll:false,
+        //页面尺寸发生改变，重新渲染页面
+        responsive:true,
+    });
+
+});
